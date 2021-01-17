754fa43f49f8343e9576d2b92b102675518a26dd
Your Name <you@example.com>
2021-01-17 07:09:46 +0530

    slice in snake Head

--- main.py
+++ main.py
@@ -62,10 +62,8 @@ while is_game_on:
         is_game_on = False
         score.game_over()
 
-    for segment in snake.segment:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segment[1:]:
+        if snake.head.distance(segment) < 10:
             is_game_on = False
             score.game_over()
 
